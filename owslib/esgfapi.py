e9a4b906a2b906cd53acc38735d42a885008bc48
Pingu Carsti <ehbrecht@dkrz.de>
2019-01-22 19:24:15 +0100

    using parameter

--- owslib/esgfapi.py
+++ owslib/esgfapi.py
@@ -1,4 +1,5 @@
 import json
+from uuid import uuid1
 
 from owslib.wps import ComplexDataInput
 
@@ -7,13 +8,32 @@ class ParameterError(Exception):
     pass
 
 
-class Variable(ComplexDataInput):
+class Parameter(ComplexDataInput):
+    def __init__(self, name=None):
+        self._name = name or uuid1().hex
+
+        super(Parameter, self).__init__(
+            value=json.dumps(self.json),
+            mimeType="application/json", encoding=None, schema=None)
+
+    @classmethod
+    def from_json(cls, data):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def json(self):
+        raise NotImplementedError
+
+
+class Variable(Parameter):
     def __init__(self, id=None, uri=None):
         self._id = id
         self._uri = uri
-        super(Variable, self).__init__(
-            value=json.dumps(self.json),
-            mimeType="application/json", encoding=None, schema=None)
+        super(Variable, self).__init__()
 
     @property
     def name(self):
